cc288b554ad047612ef89a03aba86bbb345844e9
Matvey Kovalev <matvey.kovalev@ispras.ru>
2025-10-20 23:58:13 +0300

    Вторые правки

--- static/scripts/navi.js
+++ static/scripts/navi.js
@@ -7,22 +7,39 @@ if (window.innerWidth > 900){
     down.style.top = scrollY + window.innerHeight - 32 + 'px';
 }
 var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
-up.onclick = function(event){
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+up.onclick = async function(event){
+    let n = Math.round(height/4000);
+    if(n > 0){
+        for(let i = n-1; i > 0; i--){
+        window.scrollTo({left: pageXOffset, top: Math.round(i*height/n), behavior: 'smooth'});
+        await sleep(700);
+        }
+    }
     window.scrollTo({left: pageXOffset, top: 0, behavior: 'smooth'});
 }
-down.onclick = function(event){
+down.onclick = async function(event){
     // На телефонах картинки долго загружаются и высота страницы считается без учёта этих картинок, поэтому обновляем высоту 
     height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+    let n = Math.round(height/4000);
+    if(n > 0){
+        for(let i = 1; i < n; i++){
+        window.scrollTo({left: pageXOffset, top: Math.round(i*height/n), behavior: 'smooth'});
+        await sleep(700);
+        }
+    }
     window.scrollTo({left: pageXOffset, top: height, behavior: 'smooth'});
 }
 if (window.innerWidth > 900){
     document.addEventListener('scroll', function(event){
-        up.style.top = scrollY + window.innerHeight - 180 + 'px';
-        down.style.top = scrollY + window.innerHeight - 80 + 'px';
+        up.style.top = scrollY + window.innerHeight - 190 + 'px';
+        down.style.top = scrollY + window.innerHeight - 90 + 'px';
     });
 }else{
     document.addEventListener('scroll', function(event){
-        up.style.top = scrollY + window.innerHeight - 72 + 'px';
-        down.style.top = scrollY + window.innerHeight - 32 + 'px';
+        up.style.top = scrollY + window.innerHeight - 80 + 'px';
+        down.style.top = scrollY + window.innerHeight - 40 + 'px';
     });
 }
