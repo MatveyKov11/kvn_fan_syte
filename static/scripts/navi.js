d0401436373402b2f9ea1e6223e87bbff97fa16b
Matvey Kovalev <matvey.kovalev@ispras.ru>
2025-10-20 12:56:48 +0300

    Исправление скриптов для мобилок

--- static/scripts/navi.js
+++ static/scripts/navi.js
@@ -1,6 +1,11 @@
 var body = document.body, html = document.documentElement;
-up.style.top = scrollY + window.innerHeight - 180 + 'px';
-down.style.top = scrollY + window.innerHeight - 80 + 'px';
+if (window.innerWidth > 900){
+    up.style.top = scrollY + window.innerHeight - 180 + 'px';
+    down.style.top = scrollY + window.innerHeight - 80 + 'px';
+}else{
+    up.style.top = scrollY + window.innerHeight - 36 + 'px';
+    down.style.top = scrollY + window.innerHeight - 16 + 'px';
+}
 var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
 up.onclick = function(event){
     window.scrollTo({left: pageXOffset, top: 0, behavior: 'smooth'});
@@ -10,7 +15,14 @@ down.onclick = function(event){
     height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
     window.scrollTo({left: pageXOffset, top: height, behavior: 'smooth'});
 }
-document.addEventListener('scroll', function(event){
-    up.style.top = scrollY + window.innerHeight - 180 + 'px';
-    down.style.top = scrollY + window.innerHeight - 80 + 'px';
-});
+if (window.innerWidth > 900){
+    document.addEventListener('scroll', function(event){
+        up.style.top = scrollY + window.innerHeight - 180 + 'px';
+        down.style.top = scrollY + window.innerHeight - 80 + 'px';
+    });
+}else{
+    document.addEventListener('scroll', function(event){
+        up.style.top = scrollY + window.innerHeight - 36 + 'px';
+        down.style.top = scrollY + window.innerHeight - 16 + 'px';
+    });
+}
--- static/scripts/zhuri.js
+++ static/scripts/zhuri.js
@@ -12,15 +12,30 @@ close2.onclick = function(event) {
 	window2.style.display = 'none';
 }
 
-window1.style.left = 50 + 'px';
-window1.style.width = window.innerWidth - 100 + 'px';
-window1.style.height = window.innerHeight - 100 + 'px';
+if (window.innerWidth > 900){
+	window1.style.left = 50 + 'px';
+	window1.style.width = window.innerWidth - 100 + 'px';
+	window1.style.height = window.innerHeight - 100 + 'px';
 
-window2.style.left = 50 + 'px';
-window2.style.width = window.innerWidth - 100 + 'px';
-window2.style.height = window.innerHeight - 100 + 'px';
+	window2.style.left = 50 + 'px';
+	window2.style.width = window.innerWidth - 100 + 'px';
+	window2.style.height = window.innerHeight - 100 + 'px';
 
-document.onscroll = function(event) {
-	window1.style.top = scrollY + 50 + 'px';
-    window2.style.top = scrollY + 50 + 'px';
+	document.onscroll = function(event) {
+		window1.style.top = scrollY + 50 + 'px';
+		window2.style.top = scrollY + 50 + 'px';
+	}
+}else{
+	window1.style.left = 10 + 'px';
+	window1.style.width = window.innerWidth - 20 + 'px';
+	window1.style.height = window.innerHeight - 20 + 'px';
+
+	window2.style.left = 10 + 'px';
+	window2.style.width = window.innerWidth - 20 + 'px';
+	window2.style.height = window.innerHeight - 20 + 'px';
+
+	document.onscroll = function(event) {
+		window1.style.top = scrollY + 10 + 'px';
+		window2.style.top = scrollY + 10 + 'px';
+	}
 }
